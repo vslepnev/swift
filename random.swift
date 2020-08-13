dd3144f7c054cf6fe40fa730f26854a278024fc8
vlad <vlad@localhost>
2020-08-13 23:26:17 +0300

    random machine (not done)

---
+++ random.swift
@@ -0,0 +1,11 @@
+let rand = Int.random (in: 0..<100) 
+
+func randomInt (in intRange:Range < Int >) ->  Void {
+print (Int.random (in:intRange))
+}
+
+var counter = 5
+
+while counter > 0 {
+    print(rand)
+}
